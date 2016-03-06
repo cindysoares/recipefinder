946a55e77fdd280a8ed33e8e02a70aa57b549c58
Cindy Soares <cindy.as@gmail.com>
2016-03-05 23:16:26 -0300

    fixing

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,10 +2,10 @@ class Recipe
 	include HTTParty
 
 	key_value = ENV['FOOD2FORK_KEY']
-	hostport = (raise RuntimeError, ENV['FOOD2FORK_SERVER_AND_PORT']) rescue 'www.food2fork.com'
+	hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
 
 	base_uri "http://#{hostport}/api"
-	default_params key: "aeed8c1d2d2ea1d2fc291517bce5f03c"
+	default_params key: key_value
 	format :json
 	
 	def self.for term
